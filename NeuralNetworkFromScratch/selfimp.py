7f5989c6593e251181aa65bb3b03c1078a8fe7a1
Hieu <16520396@gm.uit.edu.vn>
2020-05-18 17:10:30 +0700

    derivative

--- NeuralNetworkFromScratch/selfimp.py
+++ NeuralNetworkFromScratch/selfimp.py
@@ -16,7 +16,7 @@ class NeuralNetWork():
     def calculate_Z(self, X, weights, b):
         #print("X shapre",X.shape)
         #print('w shape', weights.shape)
-        return np.dot(X, weights) + b
+        return np.dot(X, weights) + b, (X,weights,b)
 
     def create_b_base_on_layer(self, layers):
         l = len(layers)
@@ -30,29 +30,34 @@ class NeuralNetWork():
     def foward_prop(self, X, weights, b):
         l = len(weights)
         A = X
+        caches = []
         #print(X.shape)
         for i in range(1,l+1):
-            X = self.calculate_Z(A, weights[i], b[i])
+            #print(i)
+            X, cache = self.calculate_Z(A, weights[i], b[i])
             #print('X' , X)
+            caches.append(cache)
             A = self.sigmoid(X)
             #print('A', A)
             X = A
-        return A
+        return A, caches
     def compute_cost(self, AL, Y):
         m = Y.shape[0]
         cost = -(1/m) * np.sum(np.multiply(Y, np.log(AL)) + np.multiply(1-Y, np.log(1- AL)))
         return cost
     
-    def gradient(self, dZ):
-        dw = a_prev * dZ
-        db = dZ
-        da = w * dZ
+    def gradient(self, dZ, paramater):
+        d = {}
+        d['w'] = paramater['a_prev'] * dZ
+        d['b'] = dZ
+        d['a'] = paramater['w'] * dZ
 
 obj = NeuralNetWork()
 
-weights = obj.create_structure([3,2,1])
-#print('weights',weights)
-b = obj.create_b_base_on_layer([3,2,1])
+weights = obj.create_structure([3,1])
+print('weights',weights)
+print(weights[1].shape)
+b = obj.create_b_base_on_layer([3,1])
 #print('b', b)
 
 
@@ -62,6 +67,14 @@ X= np.array([[1,2,3] ])
 Y = np.array([[1]])
 #print(obj.sigmoid(2.88))
 
-AL = obj.foward_prop(X,weights,b)
-print(AL)
+AL, caches = obj.foward_prop(X,weights,b)
+#print("caches",caches)
+dZ = AL -Y
+#
+
+#print(obj.gradient(dZ,))
+
+#print(AL)
 #print(obj.compute_cost(AL,Y))
+
+print(np.dot([1,2,3], 0.5))
