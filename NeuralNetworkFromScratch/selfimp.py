7dd169ef1616795e6ea2a30cb909933cd07a9bd7
Hieu <16520396@gm.uit.edu.vn>
2020-05-18 08:18:00 +0700

    back

--- NeuralNetworkFromScratch/selfimp.py
+++ NeuralNetworkFromScratch/selfimp.py
@@ -3,6 +3,7 @@ np.random.seed(42)
 
 class NeuralNetWork():
     layer = {}
+    b = {}
     def create_matrix_base_on_layer(self, previous_layer, current_layer):
         return np.random.rand(previous_layer, current_layer)
 
@@ -12,35 +13,55 @@ class NeuralNetWork():
             self.layer[i]  = self.create_matrix_base_on_layer(number_each_layer[i-1], number_each_layer[i])
         return self.layer
 
-    def calculate_Z(self, X, weights):
+    def calculate_Z(self, X, weights, b):
         #print("X shapre",X.shape)
         #print('w shape', weights.shape)
-        return np.dot(X, weights)
+        return np.dot(X, weights) + b
+
+    def create_b_base_on_layer(self, layers):
+        l = len(layers)
+        for i in range(1,l):
+            self.b[i] = np.random.rand()
+        return self.b
 
     def sigmoid(self, Z):
         return 1/ (1 + np.exp(-Z))
 
-    def foward_prop(self, X, weights):
+    def foward_prop(self, X, weights, b):
         l = len(weights)
         A = X
-        print(X.shape)
+        #print(X.shape)
         for i in range(1,l+1):
-            X = self.calculate_Z(A, weights[i])
+            X = self.calculate_Z(A, weights[i], b[i])
+            #print('X' , X)
             A = self.sigmoid(X)
+            #print('A', A)
             X = A
         return A
     def compute_cost(self, AL, Y):
         m = Y.shape[0]
         cost = -(1/m) * np.sum(np.multiply(Y, np.log(AL)) + np.multiply(1-Y, np.log(1- AL)))
         return cost
-            
+    
+    def gradient(self, dZ):
+        dw = a_prev * dZ
+        db = dZ
+        da = w * dZ
 
 obj = NeuralNetWork()
+
 weights = obj.create_structure([3,2,1])
+#print('weights',weights)
+b = obj.create_b_base_on_layer([3,2,1])
+#print('b', b)
 
-X= np.array([[1,2,3], [4,5,6]])
+
+
+
+X= np.array([[1,2,3] ])
 Y = np.array([[1]])
+#print(obj.sigmoid(2.88))
 
-AL = obj.foward_prop(X,weights)
-
-print(obj.compute_cost(AL,Y))
+AL = obj.foward_prop(X,weights,b)
+print(AL)
+#print(obj.compute_cost(AL,Y))
