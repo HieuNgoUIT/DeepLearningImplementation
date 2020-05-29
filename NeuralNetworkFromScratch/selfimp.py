ba44174c5fdf8112b38663d361ff3028e6f147bb
Hieu <16520396@gm.uit.edu.vn>
2020-05-29 14:24:22 +0700

    update

--- NeuralNetworkFromScratch/selfimp.py
+++ NeuralNetworkFromScratch/selfimp.py
@@ -32,13 +32,22 @@ class NeuralNetwork():
     def sigmoid(self, Z):
         return 1/ (1 + np.exp(-Z))
 
+    def relu(self ,Z):
+        return np.maximum(0,Z)
+
     def foward_prop(self, X, weights, b):
         A = X
         caches = []
-        for i in range(1, self.l):
+        current = 0
+        for i in range(1, self.l -1 ):
             Z, cache = self.calculate_Z(A, weights[i], b[i])
             caches.append(cache)
-            A = self.sigmoid(Z)
+            A = self.relu(Z)
+            current = i
+
+        Z, cache = self.calculate_Z(A, weights[current+1], b[current+1])
+        caches.append(cache)
+        A = self.sigmoid(Z)
         return A, caches
 
     def compute_cost(self, AL, Y):
@@ -62,17 +71,19 @@ class NeuralNetwork():
             d = self.gradient(dZ, caches[i-1])
             self.weights[i] -= alpha *  d['w']
             self.b[i] -= alpha * d['b']
-            #print('a',caches[i-1]['A_prev'])
-            #print('w',caches[i-1]['w'])
-            #print('b',caches[i-1]['b'])
+
             Z , _ = self.calculate_Z(caches[i-2]['A_prev'], caches[i-2]['w'], caches[i-2]['b'])
-            s = self.sigmoid(Z)
-            s1 = 1 -  self.sigmoid(Z)
-            c = s*s1
-            #print('c shape',c.shape)
-            dZ =  c * d['a'] 
-            #print(dZ.shape)
-            #print('c',dZ.shape)
+            dZ = np.array(d['a'], copy=True)
+            try:
+                dZ[Z<=0] = 0
+            except:
+                pass
+
+            
+            # s = self.sigmoid(Z)
+            # s1 = 1 -  self.sigmoid(Z)
+            # c = s*s1
+            #dZ =  c * d['a']
     def fit(self, X, Y, epoch, alpha):
         for _ in range(epoch):
             AL, caches = self.foward_prop(X, self.weights, self.b)
@@ -83,62 +94,62 @@ class NeuralNetwork():
     def predict(self, X):
         return self.foward_prop(X, self.weights,self.b)
 
-# obj = NeuralNetwork([2,1])
-# #print('shape',np.array([[1,2],[3,4] ]).shape)
-# #print('test sig', obj.sigmoid(np.array([[1,2],[3,4] ])))
+obj = NeuralNetwork([2,2,1])
+#print('shape',np.array([[1,2],[3,4] ]).shape)
+#print('test sig', obj.sigmoid(np.array([[1,2],[3,4] ])))
 
-# X= np.array([[0,1], [1,0], [1,1], [0,0]])
-# Y = np.array([[1], [1], [1], [0]])
+X= np.array([[0,1], [1,0], [1,1], [0,0]])
+Y = np.array([[0], [0], [1], [0]])
 
-# obj.fit(X,Y, 5000, 0.1)
+obj.fit(X,Y, 5000, 0.1)
 
-# lable, _ = obj.predict(np.array([[1,1]]))
-# print(lable)
-
-
-import numpy as np
-import matplotlib.pyplot as plt
-import h5py
-import scipy
-from PIL import Image
-from scipy import ndimage
-from lr_utils import load_dataset
-import scipy.misc
-
-train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+lable, _ = obj.predict(np.array([[1,1]]))
+print(lable)
 
 
-index = 7
-plt.imshow(train_set_x_orig[index])
-print ("y = " + str(train_set_y[:, index]) + ", it's a '" + classes[np.squeeze(train_set_y[:, index])].decode("utf-8") +  "' picture.")
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import h5py
+# import scipy
+# from PIL import Image
+# from scipy import ndimage
+# from lr_utils import load_dataset
+# import scipy.misc
 
-m_train = train_set_x_orig.shape[0]
-m_test = test_set_x_orig.shape[0]
-num_px = train_set_x_orig.shape[1] 
-
-
-print ("Number of training examples: m_train = " + str(m_train))
-print ("Number of testing examples: m_test = " + str(m_test))
-print ("Height/Width of each image: num_px = " + str(num_px))
-print ("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
-print ("train_set_x shape: " + str(train_set_x_orig.shape))
-print ("train_set_y shape: " + str(train_set_y.shape))
-print ("test_set_x shape: " + str(test_set_x_orig.shape))
-print ("test_set_y shape: " + str(test_set_y.shape))
+# train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
 
 
-# Reshape the training and test examples
+# index = 7
+# plt.imshow(train_set_x_orig[index])
+# print ("y = " + str(train_set_y[:, index]) + ", it's a '" + classes[np.squeeze(train_set_y[:, index])].decode("utf-8") +  "' picture.")
+
+# m_train = train_set_x_orig.shape[0]
+# m_test = test_set_x_orig.shape[0]
+# num_px = train_set_x_orig.shape[1] 
 
 
-train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0],-1)
-test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0],-1)
-train_set_y = train_set_y.T
-test_set_y= test_set_y.T
+# print ("Number of training examples: m_train = " + str(m_train))
+# print ("Number of testing examples: m_test = " + str(m_test))
+# print ("Height/Width of each image: num_px = " + str(num_px))
+# print ("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
+# print ("train_set_x shape: " + str(train_set_x_orig.shape))
+# print ("train_set_y shape: " + str(train_set_y.shape))
+# print ("test_set_x shape: " + str(test_set_x_orig.shape))
+# print ("test_set_y shape: " + str(test_set_y.shape))
 
-print ("train_set_x_flatten shape: " + str(train_set_x_flatten.shape))
-print ("train_set_y shape: " + str(train_set_y.shape))
-print ("test_set_x_flatten shape: " + str(test_set_x_flatten.shape))
-print ("test_set_y shape: " + str(test_set_y.shape))
 
-my_neural = NeuralNetwork([12288, 20, 7, 5, 1])
-my_neural.fit(train_set_x_flatten, train_set_y, 5000, 0.1)
+# # Reshape the training and test examples
+
+
+# train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0],-1)
+# test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0],-1)
+# train_set_y = train_set_y.T
+# test_set_y= test_set_y.T
+
+# print ("train_set_x_flatten shape: " + str(train_set_x_flatten.shape))
+# print ("train_set_y shape: " + str(train_set_y.shape))
+# print ("test_set_x_flatten shape: " + str(test_set_x_flatten.shape))
+# print ("test_set_y shape: " + str(test_set_y.shape))
+
+# my_neural = NeuralNetwork([12288, 20, 7, 5, 1])
+# my_neural.fit(train_set_x_flatten, train_set_y, 5000, 0.1)
---
+++ NeuralNetworkFromScratch/test.py
@@ -0,0 +1,11 @@
+def reassign(list1):
+  list1 = [0, 1]
+
+def append(list1):
+  list1.append(6)
+
+
+list1 = [Non]
+reassign(list1)
+#append(list1)
+print(list1)
